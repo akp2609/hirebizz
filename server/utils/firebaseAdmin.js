05cca511d48526f3b036dfa919f8e3d3b845fba2
Aman <amanpandey1910@gmail.com>
2025-06-14 17:21:43 +0530

    Deployement patch 1.0

--- server/utils/firebaseAdmin.js
+++ server/utils/firebaseAdmin.js
@@ -4,12 +4,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../config/firebase-key.json"), "utf-8")
+  fs.readFileSync(process.env.hirebizz-firebase-key)
 );
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
