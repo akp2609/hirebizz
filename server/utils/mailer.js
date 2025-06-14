76bd05b8a3070204065a66a2cf02c70e99e5d0d8
Aman <amanpandey1910@gmail.com>
2025-06-15 05:05:39 +0530

    Cron job patch 1.0

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: true,
+    secure: false,
     auth:{
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
