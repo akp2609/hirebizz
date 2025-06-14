0f7da6c86fbb63cd8397819c01a164fa63091d5c
Aman <amanpandey1910@gmail.com>
2025-06-15 02:17:56 +0530

    Deployement Patch 2.2.6

--- server/utils/firebaseAdmin.js
+++ server/utils/firebaseAdmin.js
@@ -8,13 +8,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const isLocal = process.env.NODE_ENV !== 'production';
-const serviceAccount = isLocal?JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../config/firebase-key.json"), "utf-8")
-): fs.readFileSync("/etc/secrets/firebase/sa/hirebizz-sa-firebase", "utf-8");
+const serviceAccount = isLocal ? JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../config/firebase-key.json"), "utf-8")
+) : JSON.parse(
+    fs.readFileSync("/etc/secrets/firebase/sa/hirebizz-sa-firebase", "utf-8")
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://hirebizz-chat-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://hirebizz-chat-default-rtdb.firebaseio.com"
 });
 
 const db = admin.database();
