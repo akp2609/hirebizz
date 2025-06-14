b38b6b477c149f0660bac3845559963ce29f4582
Aman <amanpandey1910@gmail.com>
2025-06-15 04:24:32 +0530

    - Added OIDC token verification using Google client library

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { getChat, markAsSeen, sendMessages } from '../controllers/chatController.js';
+import { checkUnseenMessages, getChat, markAsSeen, sendMessages } from '../controllers/chatController.js';
 import requireAuth from '../middleware/authMiddleware.js';
+import { verifyGoogleOIDC } from '../middleware/OIDCMiddleware.js';
 const router = express.Router();
 
 router.post('/send',requireAuth,sendMessages);
@@ -8,5 +9,7 @@ router.post('/send',requireAuth,sendMessages);
 router.get("/:user1/:user2", requireAuth,getChat);
 
 router.post('/mark-as-seen',requireAuth,markAsSeen);
+
+router.get('/check-unseen-messages',verifyGoogleOIDC,checkUnseenMessages)
 
 export default router;
