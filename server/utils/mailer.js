dc2f6ee9001e0ad67062bdb56306a7d847a203e0
Aman <amanpandey1910@gmail.com>
2025-06-18 03:09:31 +0530

    Brevo Patch 1.3.1

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -2,14 +2,13 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const BREVO_API_URL = 'https://api.brevo.com/v3/smtp/email';
-const BREVO_API_KEY = process.env.BREVO_API_KEY;
-
 
 
 export const sendVerificationEmail = async (to, token) => {
   const url = `http://localhost:3000/verify-email/${token}`;
 
+  const BREVO_API_URL = 'https://api.brevo.com/v3/smtp/email';
+  const BREVO_API_KEY = process.env.BREVO_API_KEY;
   const headers = {
     'api-key': BREVO_API_KEY,
     'Content-Type': 'application/json',
@@ -34,7 +33,8 @@ export const sendVerificationEmail = async (to, token) => {
 
 export const sendResetPasswordEmail = async (email, token) => {
   const resetURL = `http://localhost:3000/reset-password?token=${token}`;
-
+  const BREVO_API_URL = 'https://api.brevo.com/v3/smtp/email';
+  const BREVO_API_KEY = process.env.BREVO_API_KEY;
   const headers = {
     'api-key': BREVO_API_KEY,
     'Content-Type': 'application/json',
@@ -61,6 +61,8 @@ export const sendResetPasswordEmail = async (email, token) => {
 };
 
 export const sendUnseenMessagesEmail = async (email, employer) => {
+  const BREVO_API_URL = 'https://api.brevo.com/v3/smtp/email';
+  const BREVO_API_KEY = process.env.BREVO_API_KEY;
   const headers = {
     'api-key': BREVO_API_KEY,
     'Content-Type': 'application/json',
