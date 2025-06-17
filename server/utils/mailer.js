90c3b91e8c604008ca74e2e1d076f0d7c2ac2032
akp2609 <77530301+akp2609@users.noreply.github.com>
2025-06-17 15:45:51 +0000

    Deployment Patch 4.4.1

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -8,9 +8,10 @@ console.log({
 });
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false,
+    // host: process.env.EMAIL_HOST,
+    // port: Number(process.env.EMAIL_PORT),
+    // secure: false,
+    service: 'gmail',
     auth:{
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
