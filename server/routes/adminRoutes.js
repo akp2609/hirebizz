16477d618832b40ef65da7f252ca2b898b17b139
Aman <amanpandey1910@gmail.com>
2025-07-03 17:03:36 +0530

    Patch

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import requireAuth from '../middleware/authMiddleware.js';
 import { requireAdmin } from '../middleware/requireAdmin.js';
 import { requireSuperAdmin } from '../middleware/requireSuperAdmin.js';
-import { assignUserRole, deleteUser, getAllJobs, getAllUsers, updateJobStatus } from '../controllers/adminController.js';
+import { assignUserRole, deleteUser, getAllJobs, getAllUsers, getUserById, updateJobStatus } from '../controllers/adminController.js';
 import { authLimiter } from '../utils/authLimiter.js';
 const router = express.Router();
 
@@ -15,5 +15,7 @@ router.get('/get-all-jobs',requireAuth,requireAdmin,getAllJobs)
 router.patch('/update-job-status',requireAuth,requireAdmin,updateJobStatus);
 
 router.delete('/delete-user',requireAuth,requireAdmin,authLimiter,deleteUser);
+
+router.get('/get-user/:userId',requireAuth,requireAdmin,getUserById);
 
 export default router;
