0280f25513fadc5b24d18710a2893efad979a7a2
Aman <amanpandey1910@gmail.com>
2025-07-23 15:40:15 +0530

    fix(apply): chat thread fix

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -4,14 +4,15 @@ import requireAuth from '../middleware/authMiddleware.js';
 import { verifyGoogleOIDC } from '../middleware/OIDCMiddleware.js';
 const router = express.Router();
 
-router.post('/send',requireAuth,sendMessages);
-
-router.get("/:user1/:user2", requireAuth,getChat);
-
-router.patch('/mark-as-seen',requireAuth,markAsSeen);
-
 router.get('/check-unseen-messages',verifyGoogleOIDC,checkUnseenMessages)
 
 router.get('/user-threads/:userId', requireAuth, getUserChatThreads);
+
+router.get("/thread/:user1/:user2", requireAuth,getChat);
+
+router.post('/send',requireAuth,sendMessages);
+
+router.patch('/mark-as-seen',requireAuth,markAsSeen);
+
 
 export default router;
