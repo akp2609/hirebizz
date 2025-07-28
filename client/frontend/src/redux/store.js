8616632e4d1a53290a4bd7b3f5175db00a8d929c
Aman <amanpandey1910@gmail.com>
2025-07-28 17:07:31 +0530

    fix(add): notificaiton bell fix

--- client/frontend/src/redux/store.js
+++ client/frontend/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import chatReducer from './slices/chatSlice';
+import chatReducer from '../redux/slices/ChatSlice';
 
 export const store = configureStore({
     reducer: {
