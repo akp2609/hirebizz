c863bf95f57acc128f6c9854b1c9dc71ec89c24d
Aman <amanpandey1910@gmail.com>
2025-08-02 07:30:25 +0530

    fix(fix): navbar

--- client/frontend/src/context/UserContext.js
+++ client/frontend/src/context/UserContext.js
@@ -1,20 +1,21 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { getUserProfile, refreshUserResumeURL } from "../services/userService";
 
-
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+        const storedUser = localStorage.getItem("user");
+        return storedUser ? JSON.parse(storedUser) : null;
+    });
+
     const [loadingUser, setLoadingUser] = useState(true);
 
     const loadUser = async () => {
         try {
             const data = await getUserProfile();
 
-
             let updatedUser = data.user;
-
 
             if (data.user?.objectName) {
                 try {
@@ -28,25 +29,26 @@ export const UserProvider = ({ children }) => {
                 }
             }
 
-            setUser(data.user);
-        }
-        catch (err) {
+            setUser(updatedUser);
+            
+            localStorage.setItem("user", JSON.stringify(updatedUser));
+        } catch (err) {
             console.error("Failed to load user: ", err);
+            localStorage.removeItem("user");
         } finally {
             setLoadingUser(false);
         }
-    }
+    };
 
     useEffect(() => {
         loadUser();
     }, []);
 
     return (
-        <UserContext.Provider value={{ user, loadingUser,reloadUser: loadUser }}>
+        <UserContext.Provider value={{ user, loadingUser, reloadUser: loadUser }}>
             {children}
         </UserContext.Provider>
-    )
+    );
 };
-
 
 export const useUser = () => useContext(UserContext);
