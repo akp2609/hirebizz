fa32c04706fec3cdb422e7e8ec4aaf8a841c135f
Aman <amanpandey1910@gmail.com>
2025-08-02 07:53:50 +0530

    fix(fix): navbar

--- client/frontend/src/components/layout/NavBar.js
+++ client/frontend/src/components/layout/NavBar.js
@@ -2,27 +2,25 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import NotificationBell from '../ui/Notification';
-import { useUser } from '../../context/UserContext';
 
 function NavBar() {
-    const { isAuthenticated, user, logout } = useContext(AuthContext);
+    const { isAuthenticated, user, logout, loading } = useContext(AuthContext);
     const [isOpen, setIsOpen] = useState(false);
-
     const defaultProfilePic = "https://www.w3schools.com/howto/img_avatar.png";
+
+    if (loading) return null; 
 
     return (
         <nav className="bg-blue-700 text-white px-7 py-6 shadow-md flex justify-between relative z-50">
             <h1 className='text-2xl font-bold'>HireBizz</h1>
             <div className='space-x-4 flex'>
-
-
                 {isAuthenticated && user ? (
                     <div
                         className="inline-flex items-center cursor-pointer gap-4"
                         onMouseEnter={() => setIsOpen(true)}
                         onClick={() => setIsOpen(!isOpen)}
                     >
-                        <NotificationBell userId={user._id}/>
+                        <NotificationBell userId={user._id} />
                         <Link to='/' className='hover:text-cyan-300 hover:underline text-xl'>Home</Link>
                         <Link to='/about' className='hover:underline hover:text-cyan-300 text-xl'>About</Link>
                         <img
@@ -34,32 +32,28 @@ function NavBar() {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
                         </svg>
 
-
-
                         {isOpen && (
-                            <div className="absolute right-0 overflow-hidden top-16 w-40 bg-white border rounded-md shadow-lg py-2 z-50 items-center mt-2">
-                                <p className='text-black flex justify-center items-center'>
-                                    {user.name || "User"}
-                                </p>
-                                <hr style={{ border: '1px solid gray' }} className='mt-2' />
+                            <div className="absolute right-0 top-16 w-40 bg-white border rounded-md shadow-lg py-2 z-50 items-center mt-2">
+                                <p className='text-black flex justify-center items-center'>{user.name || "User"}</p>
+                                <hr className='mt-2 border-gray-300' />
                                 <Link to="/profile" className='text-black flex justify-center items-center mt-2 hover:bg-gray-100'>Profile</Link>
                                 {user.role !== 'employer' && (
                                     <>
-                                        <hr style={{ border: '1px solid gray' }} className='mt-2' />
+                                        <hr className='mt-2 border-gray-300' />
                                         <Link to="/saved-jobs" className='text-black flex justify-center items-center mt-2 hover:bg-gray-100'>Saved Jobs</Link>
                                     </>
                                 )}
                                 {user.role === 'candidate' && (
                                     <>
-                                        <hr style={{ border: '1px solid gray' }} className='mt-2' />
+                                        <hr className='mt-2 border-gray-300' />
                                         <Link to="/my-applications" className='text-black flex justify-center items-center mt-2 hover:bg-gray-100'>My Applications</Link>
                                     </>
                                 )}
-                                <hr style={{ border: '1px solid gray' }} className='mt-2' />
+                                <hr className='mt-2 border-gray-300' />
                                 <Link to='/setting' className='text-black flex justify-center items-center mt-2 hover:bg-gray-100'>Setting</Link>
-                                <hr style={{ border: '1px solid gray' }} className='mt-2' />
+                                <hr className='mt-2 border-gray-300' />
                                 <Link to='/chats' className='text-black flex justify-center items-center mt-2 hover:bg-gray-100'>Chats</Link>
-                                <hr style={{ border: '1px solid gray' }} className='mt-2' />
+                                <hr className='mt-2 border-gray-300' />
                                 <button
                                     onClick={logout}
                                     className="w-full text-left px-4 hover:bg-gray-100 text-black flex justify-center items-center"
