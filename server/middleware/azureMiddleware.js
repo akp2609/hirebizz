700f2fb295197d2ade963619c3c9797034e768b2
Aman <amanpandey1910@gmail.com>
2025-08-12 16:49:17 +0530

    add(fix): jwt import

--- server/middleware/azureMiddleware.js
+++ server/middleware/azureMiddleware.js
@@ -1,11 +1,11 @@
-import jwt from 'express-jwt';
+import {expressjwt} from 'express-jwt';
 import jwksRsa from 'jwks-rsa';
 
 const tenantId = process.env.AZURE_TENANT_ID;
 const clientId = process.env.AZURE_API_CLIENT_ID;
 const audience = process.env.AZURE_AUDIENCE;
 
-export const checkJwt = jwt({
+export const checkJwt = expressjwt({
     secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
@@ -13,6 +13,6 @@ export const checkJwt = jwt({
         jwksUri: `https://login.microsoftonline.com/${tenantId}/discovery/v2.0/keys`
     }),
     audience: audience,
-    isssuer:`https://login.microsoftonline.com/${tenantId}/v2.0`,
+    issuer:`https://login.microsoftonline.com/${tenantId}/v2.0`,
     algorithms: ['RS256']
 })
