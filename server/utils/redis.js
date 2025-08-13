1b548944b687b184c87c655a1ffc1550cf24cd02
Aman <amanpandey1910@gmail.com>
2025-08-13 07:11:08 +0530

    fix(fix): redis connection

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,15 +1,20 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
+
     username: 'default',
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT)
+        port: Number(process.env.REDIS_PORT),
+        tls: true
     }
 });
 
 redisClient.on('error', err => console.log('Redis Client Error', err));
+
+console.log("REDIS_HOST:", process.env.REDIS_HOST);
+console.log("REDIS_PORT:", process.env.REDIS_PORT);
 
 await redisClient.connect();
 
