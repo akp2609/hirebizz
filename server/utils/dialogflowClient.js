edef0f6375084e78bfe610390fc679b3fa6c735a
akp2609 <77530301+akp2609@users.noreply.github.com>
2025-09-01 03:28:02 +0000

    dialogflow backend integration fix

--- server/utils/dialogflowClient.js
+++ server/utils/dialogflowClient.js
@@ -6,6 +6,8 @@ import fs from 'fs';
 const keyPath = '/etc/secrets/dialogflow/dialogflow-key';
 const credentials = JSON.parse(fs.readFileSync(keyPath,'utf8'));
 
-const client = new SessionsClient({credentials});
+const client = new SessionsClient({credentials,
+    apiEndpoint: 'us-central1-dialogflow.googleapis.com'
+});
 
 export default client;
